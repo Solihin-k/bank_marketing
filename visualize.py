b1c6456ef1ef690854fd39cf8c251526944d1f9d
Solihin Karsani <solihin.karsani@gmail.com>
2020-10-22 09:25:49 +0800

    updated functions

--- visualize.py
+++ visualize.py
@@ -29,37 +29,49 @@ def plot_hist(df, x_col, target_col):
 	return new_plot
 
 
-
 def plot_bar(df, x_col, target_col):
 
-	if x_col == 'day':
-		new_df = pd.crosstab(df[x_col], df[target_col], normalize = 'all')
-		new_df.plot(kind = 'bar', figsize = (14, 6))
-		plt.xticks(range(0, len(new_df.index)), new_df.index, fontsize = 12)
-		plt.xlabel('Day-of-Month', fontsize = 12)
-		plt.ylabel('Frequency', fontsize = 12)
-		plt.title('Subscription Outcome by Day-of-Month', fontsize = 18)
+	"""
+	Plot bar chart.
 
-	elif x_col == 'job':
+	Keyword arguments:
+	df -- the dataframe to reference from
+	x_col -- the variable to visualise
+	target_col -- the target variable
+	"""
+
+	if x_col == 'day_of_month':
+		new_df = pd.crosstab(df[x_col], df[target_col], normalize = 'all')
+
+
+	elif x_col == 'job_type':
 		new_df = pd.crosstab(df[x_col], df[target_col], normalize = 'all').sort_values(by = 1, ascending = False)
-		new_df.plot(kind = 'bar', figsize = (14, 6))
-		plt.xticks(range(0, len(new_df.index)), new_df.index, fontsize = 12)
-		plt.xlabel('Job Type', fontsize = 12)
-		plt.ylabel('Frequency', fontsize = 12)
-		plt.title('Subscription Outcome by Job Type', fontsize = 18)
+
 
 	else:
 		new_df = pd.crosstab(df[x_col], df[target_col], normalize = 'all').reindex(months)
-		new_df.plot(kind = 'bar', figsize = (14, 6))
-		plt.xticks(range(0, len(new_df.index)), new_df.index, fontsize = 12)
-		plt.xlabel('Month-of-Year', fontsize = 12)
-		plt.ylabel('Frequency', fontsize = 12)
-		plt.title('Subscription Outcome by Month-of-Year', fontsize = 18)
+
+
+	new_df.plot(kind = 'bar', figsize = (14, 6))
+	plt.xticks(range(0, len(new_df.index)), new_df.index, fontsize = 12)
+	plt.xlabel(str(x_col).capitalize(), fontsize = 12)
+	plt.ylabel('Frequency', fontsize = 12)
+	plt.title('Subscription Outcome by ' + str(x_col).capitalize(), fontsize = 18)	
 
 	return new_df
 
 
 def subplot_bar(df, target_col, ncol):
+
+	"""
+	Plot multiple bar charts in subplot.
+
+	Keyword arguments:
+	df -- the dataframe to reference from
+	x_col -- the variable to visualise
+	target_col -- the target variable
+	ncol -- number of columns in the subplot
+	"""
 
 	index = 0
 
@@ -71,7 +83,8 @@ def subplot_bar(df, target_col, ncol):
 		for col in var_2:
 
 			new_df = pd.crosstab(df[col], df[target_col], normalize = 'all').sort_values(by = 1, ascending = False)
-			new_df.plot(kind = 'bar', ax=axes[index], fontsize = 14, title = str(col).capitalize())
+			new_df.plot(kind = 'bar', ax=axes[index], fontsize = 12, title = str(col).capitalize())
+			axes[index].title.set_size(14)
 
 			print(new_df, '\n')
 
@@ -83,8 +96,8 @@ def subplot_bar(df, target_col, ncol):
 
 			new_df = pd.crosstab(df[col], df[target_col], normalize = 'all').sort_values(by = 1, ascending = False)
 			new_df.plot(kind = 'bar', ax=axes[index], fontsize = 14, title = str(col).capitalize())
+			axes[index].title.set_size(14)
 
 			print(new_df, '\n')
 
 			index += 1
-
